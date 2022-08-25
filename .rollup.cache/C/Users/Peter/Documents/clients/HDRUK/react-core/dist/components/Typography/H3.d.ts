7617b9c2a032c163b63d794dc610a8b1bbb0fb9e
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-08-25 11:19:04 +0100

    Remove rollup

--- .rollup.cache/C/Users/Peter/Documents/clients/HDRUK/react-core/dist/components/Typography/H3.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { TypographyProps } from "./Typography.types";
-declare const H3: (props: Omit<TypographyProps, "variant">) => JSX.Element;
-export default H3;
--- .rollup.cache/C/Users/Peter/Documents/clients/HDRUK/react-core/dist/components/Typography/Tiny.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { TypographyProps } from "./Typography.types";
-declare const Tiny: (props: Omit<TypographyProps, "variant">) => JSX.Element;
-export default Tiny;
